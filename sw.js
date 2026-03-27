4c748d657667b92a03b5086607b2aa7012d67733
HilH-2026 <hillaryhuddleston@gmail.com>
2026-03-27 13:57:10 -0700

    Add files via upload
    Add website files

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'cofk-airway-v1';
+const ASSETS = [
+  '/index.html',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(names =>
+    Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    })).catch(() => caches.match('/index.html'))
+  );
+});
